7a7b5b474dc6c0aee3404d54dbfd143abd24277b
Sanat Dash <sanat.dash@comviva.com>
2025-05-30 04:04:16 +0530

    Initial Commit for Interact backend application

---
+++ src/main/java/com/interact/interactManagement/InteractManagementApplication.java
@@ -0,0 +1,13 @@
+package com.interact.interactManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interact/interactManagement/InteractManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interact.interactManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
